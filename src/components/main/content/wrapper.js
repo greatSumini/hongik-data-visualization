c09a0537701c9f7cb3e7731db506e8273ed51971
Choi Sumin <greatSumini@gmail.com>
2019-12-04 02:01:39 +0900

    [Feature] add main content wrapper - 나중에 hoc로 refactor 예정

---
+++ src/components/main/content/wrapper.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+import { useScrollPosition } from "../../scroll";
+
+export default function ContentWrapper({ children }) {
+  const scrollPosition = useScrollPosition();
+  return (
+    <FixedWrapper
+      position={scrollPosition}
+      style={{ bottom: Math.min(scrollPosition - window.innerHeight, 0) }}
+    >
+      <Wrapper>{children}</Wrapper>
+    </FixedWrapper>
+  );
+}
+
+const FixedWrapper = styled.div`
+  position: fixed;
+  right: 0;
+  width: calc(100% - 36rem);
+  height: 100vh;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  flex: 1;
+  width: 100%;
+  height: 100%;
+`;
