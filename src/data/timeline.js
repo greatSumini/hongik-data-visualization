2acaf251a2d027b9704a3d91ced02322389b49e0
Choi Sumin <greatSumini@gmail.com>
2019-12-04 01:59:20 +0900

    Fix typo

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -4,12 +4,12 @@ export default {
       title: "일본군 ‘위안부’ 피해 발생",
       content: [
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "1931년 만주사변을 일으킨 이후 일본은 침략전선을 확장해 나갔다. 그들은 점령지에 현지여성에 대한 강간 방지, 매춘에 의한 성병 예방, 그리고 병사들의 성적(性的)인 위로를 명목으로 ‘위안소’를 설치했다. 이러한 위안소에는 당시 일본의 식민지였던 조선과 대만의 여성들이 위안부로 존재했다. "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text:
             "이들은 일본 군인들에게 강제로 성을 착취당했을 뿐만 아니라, 폭력 행위의 대상이 되었다."
         }
@@ -21,20 +21,20 @@ export default {
       title: "일본군 ‘위안부’ 피해 발생",
       content: [
         {
-          hirighted: false,
+          highlighted: false,
           text: "1937년 중일전쟁을 계기로 "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text: "위안소의 수는 급증했으며, 더 많은 조선의 여성들이 강제동원 "
         },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "되었을 뿐만 아니라 다양한 국가로부터 여성들이 동원되었다. 여기에는 점령지인 중국, 필리핀, 인도네시아, 베트남, 미얀마, 그리고 그곳에 거주하던 네덜란드 여성들까지 포함된다. 일본군의 점령지 확대에 따라 더 많은 지역에 위안소가 설치되었는데, 황선순 할머니의 증언을 통해 남태평양에 위치한 나우루섬까지도 위안부를 동원했음을 알 수 있다. 일본군의 주도와 일본정부기관, 조선총독부, 그리고 대만총독부의 협력 하에 "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text:
             "고향으로부터 멀리 떨어진 곳으로 끌려간 여성들은 귀향에도 어려움을 겪었으며, 돌아오지 못하고 그곳에 남기도 했다."
         }
@@ -46,17 +46,17 @@ export default {
       title: "일본군 ‘위안부’ 피해 발생",
       content: [
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "일본군 위안소는 중국 130개소, 일본 오키나와 130개소, 필리핀 30개소, 버마 50개소, 인도네시아 40개소, 라바울 6개소 등이 존재했다. 총 동원된 위안부들의 수는 정확히 알 수 없으나 연구자에 따라 최소 3만명에서 최대 40만명까지로 추측된다. 이 문제를 오랫동안 연구한 연구자인 요시미 요시아키는 "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text:
             "위안부 수를 최소 8만 명에서 20만명으로 추산, 총 인원 중 조선 여성들의 비율이 절반이 넘는다는 결과를 발표했다. "
         },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "이렇듯 수많은 여성들, 특히 우리나라 여성들은 일본군 위안부로 끌려가 갖은 고초를 겪어야 했다."
         }
@@ -68,26 +68,26 @@ export default {
       title: "광복",
       content: [
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "1945년 8월 15일, 일제가 2차 세계대전에서 패하며 조선은 광복을 맞이하게 되었다. 위안부들 또한 자유의 몸이 되었으며, 위안소를 벗어날 수 있게 되었다. 하지만 "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text: "먼 타국으로 끌려갔기에 귀국은 쉽지 않았다. "
         },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "연합군의 포로가 되어 포로수용소에 수용되었다가 귀국하기도 했지만, 대다수는 현지에 버려지듯 남겨졌다. "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text:
             "여성들은 혼자 힘으로 살아남거나 고향에 돌아올 방법을 찾아야 했다. "
         },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "돌아갈 방법을 구하지 못하거나 고향에 돌아갈 면목이 없다는 이유로 강제동원되었던 국가에 남는 이들도 있었다. "
         }
@@ -99,25 +99,25 @@ export default {
       title: "고향으로 돌아온 위안부",
       content: [
         {
-          hirighted: false,
+          highlighted: false,
           text: "고국으로 돌아온 이들의 고생은 끝나지 않았다. "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text: "이들은 심각한 신체적, 정신적 휴유증을 앓게 되었다. "
         },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "구타 및 가혹행위로 인한 외상, 불임, 성병 등으로 고통받기도 했으며, 모욕감과 우울증, 사회에서 피해 사실로 인해 받아야 할 불이익과 낙인에 대한 두려움에 시달렸다. 이처럼 피해자의 고통보다 ‘명예’를 우선시하고, 여성의 정절을 중요시하는 "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text:
             "당시 사회 분위기에 따라 대다수의 피해자들이 자신의 피해 사실을 차마 알리지 못했다. "
         },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "심지어는 다시 찾은 가족에게까지 위안부 피해 사실을 숨기고, 공장에서 노동을 하다 풀려났다는 등의 이유를 대야 했다."
         }
@@ -130,38 +130,38 @@ export default {
       title: "김학순 할머니, 한국 내 최초 일본군 ‘위안부’ 피해자 증언",
       content: [
         {
-          hirighted: true,
+          highlighted: true,
           text:
             "김학순 할머니의 증언은 일본군 위안부 피해에 대한 최초의 생존자 증언이었다. "
         },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "공개 증언에 대한 이유는 일본 정부가 위안부의 존재를 부정하는 것을 뉴스에서 본 후 거짓말을 바로잡아야겠다는 다짐을 했기 때문이라고 밝혔다. "
         },
-        { hirighted: false, text: "" },
+        { highlighted: false, text: "" },
         {
-          hirighted: true,
+          highlighted: true,
           text:
             "기자회견에서 김학순 할머니는 일제의 만행에 대해 말했다. “내 팔을 끌고 이리 따라오라고. 무서우니까, 안 가려고 반항을 하니까 발길로 차면서 내 말을 잘 들으면 너는 편할 것이고 내 말에 반항하면 너는 여기서 죽는거야. "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text:
             "그 때 결국 그 사람한테… 참 계집애가 이 꽉 물고 강간을 당하는…. 그 참혹한….” "
         },
         {
-          hirighted: false,
+          highlighted: false,
           text: "이라며 자신의 피해를 증언했다. "
         },
-        { hirighted: false, text: "" },
+        { highlighted: false, text: "" },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "할머니는 “절대 이것은 알아야 합니다. 알아야 하고, 과거에 이런 일이 있었으니까.”라며 중요성을 강조했다. "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text: "이를 계기로 위안부 문제가 수면 위로 올라오게 되었다."
         }
       ]
@@ -171,35 +171,35 @@ export default {
       title: "도쿄지방재판소에 일본군 ‘위안부’ 문제 제소",
       content: [
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "김학순 할머니를 비롯한 3명의 일본군 위안부 피해자, 군인, 군속과 그 유족 등을 포함한 41명은 일본 정부의 사죄와 배상을 요구하며 도쿄지방재판소에 제소했다. "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text: "이는 우리나라 위안부 피해자가 제기한 최초의 소송이다."
         },
-        { hirighted: false, text: "" },
+        { highlighted: false, text: "" },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "이들은 직접 제출한 소장에서 일본이 식민 통치 기간에 수많은 조선인들을 강제동원하여 희생시킨 뒤, 전후에 이로 인한 정신적·육체적 고통과 보상을 외면해 온 행위는 ‘뉘른베르크 군사재판에서 나치전범 처벌에 적용된 이후 국제관습으로 확립된 "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text: "반인도죄에 해당한다’고 지적하고, "
         },
         {
-          hirighted: false,
+          highlighted: false,
           text: "1인당 2천만 엔씩 총 7억 엔(한화로 약 40억원)의 "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text: "보상금을 지불하라고 요구했다."
         },
-        { hirighted: false, text: "" },
+        { highlighted: false, text: "" },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "이 재판은 거듭되는 손해배상 기각을 거쳐 2004년 일본 최고재판소에서의 소송까지 이어지게 되었다. "
         }
@@ -212,37 +212,37 @@ export default {
       title: "일본 정부에서 ‘군대위안부 증언청취단’ 파견",
       content: [
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "일본이 군 위안부 동원의 강제성을 인정한 고노 담화 발표 후, 한국과 일본은 군위안부 상황을 파악하기로 합의했다. "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text:
             "이에 따라 일본 정부는 생존자들의 증언을 수집하기 위해 ‘군대위안부 증언청취단’ 9명을 파견했다. "
         },
-        { hirighted: false, text: "" },
+        { highlighted: false, text: "" },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "이들은 5일간 우리나라 위안부 피해자들로부터 직접 증언을 들었다. 당시에는 국내에서조차 위안부 상황이 파악되어 있지 않아, 태평양전쟁 희생자유족회를 통해 파악한 군대위안부 생존자는 45명에 불과했다. "
         },
-        { hirighted: false, text: "" },
+        { highlighted: false, text: "" },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "유족회 등은 생존자를 파악해 적극적인 증언을 요청했으나, 현재와는 위안부를 바라보는 시선이 달랐기에 많은 사람들이 이를 꺼려했다. 그 때 "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text: "가장 먼저 증언에 나선 사람이 김복선 할머니였다. "
         },
         {
-          hirighted: false,
+          highlighted: false,
           text: "김복선 할머니를 포함해 "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text: "총 16분이 직접 증언을 제공하게 되었다."
         }
       ]
@@ -254,36 +254,36 @@ export default {
       title: "일본 최고재판소, ‘관부재판’ 각하 결정",
       content: [
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "1992년부터 6년간 일본 시모노세키와 부산을 오가며 일본 정부를 상대로 한 법정 투쟁인 관부재판이 있었다. "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text:
             "이는 일본 정부를 상대로 벌인 수많은 법정 투쟁 가운데 유일하게 일부 승소를 받아낸 재판이다. "
         },
-        { hirighted: false, text: "" },
+        { highlighted: false, text: "" },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "당시 일본 재판부는 1심 판결에서 위안부 피해자 3명에게 각각 30만 엔(약 300만 원)씩 지급할 것을 정부 측에 명령했으나 피해자에 대한 공식 사죄 요청은 인정하지 않았다. 그러나 이러한 1심 판결에 대하여 일본정부가 항소함으로써 히로시마 고등재판소에 회부된 지 만 2년 여 만에 나온 2심 판결과 최종판결은 부결이었다. "
         },
-        { hirighted: false, text: "" },
+        { highlighted: false, text: "" },
         {
-          hirighted: false,
+          highlighted: false,
           text: "이처럼 5년간 계속된 항소와 상고 끝에 "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text: "최고재판소에서 1심 판결이 뒤집히며 "
         },
         {
-          hirighted: false,
+          highlighted: false,
           text: "관부재판에 참여한 할머니들은 "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text: "일본 정부로부터 아무런 사죄나 보상을 받지 못하게 되었다."
         }
       ]
@@ -293,38 +293,38 @@ export default {
       title: "송신도 할머니 1993년 제소 건, 일본 최고재판소 기각",
       content: [
         {
-          hirighted: true,
+          highlighted: true,
           text:
             "송신도 할머니는 1993년 4월, 당시 일본에 생존해 있던 위안부 피해자 중 유일하게 일본 정부를 상대로 재판 투쟁을 시작했다. "
         },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "도쿄 지방재판소는 송신도가 위안부 생활을 강요받은 사실은 인정하면서도 원고 청구를 기각했다. 그리고 "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text:
             "결국 2003년, 일본 최고재판소의 기각 판결로 10년 동안 벌인 재판에서 패소"
         },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "하게 되었다.할머니는 소송 이유에 대해 “일본이 저지른 전쟁에 왜 조선의 아이들이 끌려가 그런 고생을 해야 했는지 아무리 생각해도 납득이 안 갔다. 전쟁터에 끌고 갈 때는 ‘나라를 위해서’라고 해놓고선, 지금에 와서는 ‘조선인’이라고, ‘위안부’라고 차별을 한다. "
         },
-        { hirighted: false, text: "" },
+        { highlighted: false, text: "" },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "할머니는 소송 이유에 대해 “일본이 저지른 전쟁에 왜 조선의 아이들이 끌려가 그런 고생을 해야 했는지 아무리 생각해도 납득이 안 갔다. 전쟁터에 끌고 갈 때는 ‘나라를 위해서’라고 해놓고선, 지금에 와서는 ‘조선인’이라고, ‘위안부’라고 차별을 한다. "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text:
             "내가 왜 ‘위안부’가 돼야 했는지, 왜 그런 차별을 받아야 했는지 알고 싶어서 소송을 제기했다.” "
         },
         {
-          hirighted: false,
+          highlighted: false,
           text: "라고 밝혔다."
         }
       ]
@@ -336,30 +336,30 @@ export default {
       title: "‘아시아태평양전쟁 한국인 희생자 보상청구 사건’ 소송 기각",
       content: [
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "일본 최고재판소는 일제가 저지른 침략전쟁에 강제동원됐던 한국인 피해자와 유가족 35명이 일본 국가를 상대로 제기한 청구 소송을 "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text:
             "처음 이를 도쿄지방재판소에 제소한 1991년 이후 13년만에 기각했다. "
         },
-        { hirighted: false, text: "" },
+        { highlighted: false, text: "" },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "‘전쟁 피해와 희생에 대한 보상은 헌법이 전혀 예상할 수 없던 것’, ‘이번 사안은 단순히 정책적 견지에서 배려 여부를 고려할 수 있을 뿐인 사안’이라고 기각이유를 밝혔다. "
         },
-        { hirighted: false, text: "" },
+        { highlighted: false, text: "" },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "판결 직후 방청석에 있던 태평양전쟁 희생자 유족회 등의 원고들은 재판정으로 뛰어들어가 “판결은 무효, 비인도적 판결에 불복한다”며 강한 울분을 토해냈다. "
         },
-        { hirighted: false, text: "" },
+        { highlighted: false, text: "" },
         {
-          hirighted: true,
+          highlighted: true,
           text:
             "이 소송을 계기로 60여건에 달하는 아시아 각국의 전후 보상 소송이 잇따라 제기되었고, 유엔 인권위의 위안부 관련 보고서에 영향을 미치는 등 국제사회의 큰 주목을 끌었다. "
         }
@@ -372,28 +372,28 @@ export default {
       title: "미국 하원, “일본군 ‘위안부’ 결의안” 만장일치 통과",
       content: [
         {
-          hirighted: false,
+          highlighted: false,
           text: "미 하원이 2차 세계대전 당시 일본군의 위안부 강제동원과 관련, "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text:
             "일본 정부에게 공식적이고 분명한 시인과 사과, 역사적 책임을 요하는 결의안을 표결 없이 만장일치로 통과시켰다. "
         },
-        { hirighted: false, text: "" },
+        { highlighted: false, text: "" },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "결의안은 위안부 문제를 20세기 최대의 인신매매 사건 중 하나로 규정했으며, 일본의 새로운 교과서들은 전쟁범죄를 축소하려 하고 있다고 비판했다. 또한, 일본 정부의 위안부 시인, 사과 방법과 관련해 진실성에 대한 의혹이 해소되도록 일본 총리가 공식 성명을 통해 발표할 것을 권고했다. "
         },
-        { hirighted: false, text: "" },
+        { highlighted: false, text: "" },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "미 의회가 일본군 위안부 결의안을 채택한 것은 처음이며, 미국이 직접 개입되지 않은 문제에 대해 결의안을 채택한 것 또한 이례적이다. 이 결의안은 법적 구속력은 없지만, "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text:
             "위안부 강제동원을 부인하는 일본 정부 주장의 부당성을 미국 의회가 공식 확인했다는 점에서 큰 의미를 가진다."
         }
@@ -406,32 +406,32 @@ export default {
       title: "헌법재판소, 대한민국 정부의 구체적인 노력 부재 “위헌”",
       content: [
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "일본군 위안부 피해자의 배상청구권을 놓고 한일 양국 간 분쟁이 있음에도 정부가 이를 해결하기 위해 구체적인 노력을 다하지 않은 것은 "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text: "피해자의 기본권을 침해한 것으로, 헌법에 위배"
         },
         {
-          hirighted: false,
+          highlighted: false,
           text: "된다는 헌법재판소 판단이 나왔다. "
         },
-        { hirighted: false, text: "" },
+        { highlighted: false, text: "" },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "헌재는 30일에 ‘정부가 한일 간 재산 및 청구권과 관련한 분쟁을 해결하려는 조치를 취하지 않아 기본권을 침해당했다'며 위안부 피해자들이 국가의 한일청구권협정 제3조 부작위(不作爲, 마땅히 해야 할 일을 하지 않음)에 대해 낸 헌법소원심판 사건에서 위헌을 결정했다. "
         },
-        { hirighted: false, text: "" },
+        { highlighted: false, text: "" },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "이번 결정은 국가가 직접 피해자의 기본권을 침해한 것은 아니라 배상책임을 지지는 않지만, "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text:
             "향후 분쟁해결 절차에 따라 외교적 노력을 다해야 할 의무가 있음을 분명히 밝혔다."
         }
@@ -444,45 +444,45 @@ export default {
       title: "한일 위안부 문제 협상 합의 타결",
       content: [
         {
-          hirighted: false,
+          highlighted: false,
           text: "박근혜 정부와 아베 신조 일본 정부는 한일 외교장관 회담을 열고 "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text: "일본군 위안부 피해자 문제의 해결 방안에 합의"
         },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "합의했으며, 윤병세 대한민국 외교부 장관과 기시다 후미오 일본 외무대신은 그날 오후에 정부서울청사별관에서 외교장관 회담을 연 뒤 공동기자회견을 통해 합의사항을 발표하였다. "
         },
-        { hirighted: false, text: "" },
+        { highlighted: false, text: "" },
         {
-          hirighted: false,
+          highlighted: false,
           text: "이를 통해 양국 정부는 "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text:
             "일본군 위안부 문제가 최종적으로, 불가역적으로 종결되었음을 선포"
         },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "하였다. 그러나 합의 직후부터 내용의 문제, 과정의 이면 합의와 위안부 피해자 배제 논란, 일본의 진정성 문제 등이 끊임없이 제기되었다. "
         },
-        { hirighted: false, text: "" },
+        { highlighted: false, text: "" },
         {
-          hirighted: true,
+          highlighted: true,
           text: "위안부 할머니들은 합의 무효를 주장하며, "
         },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "일본 정부가 출연키로 한 10억엔 역시 받지 않기로 결의했다. 또한, 협상 결과 일본이 법적인 책임을 인정하지 않았다며 "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text: "일본의 법적 배상과 진정한 공식 사죄를 촉구했다."
         }
       ]
@@ -494,36 +494,36 @@ export default {
       title: "외교부, 한일 위안부 문제 합의 태스크포스 발족",
       content: [
         {
-          hirighted: false,
+          highlighted: false,
           text: "외교부는  "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text:
             "“위안부 TF는 위안부 합의 관련 협의 경과 및 합의 내용 전반에 대해 사실관계를 확인하고 평가"
         },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "하기로 했으며, 이 과정에서 위안부 피해자 및 관계자들의 의견도 청취해 나가기로 했다”고 설명했다. "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text:
             "이에 따라 위안부 TF의 점검 결과는 향후 합의 재협상 여부 등에 대한 정부 입장을 결정하는 데 중요한 참고 자료가 될 것으로 예상된다. "
         },
-        { hirighted: false, text: "" },
+        { highlighted: false, text: "" },
         {
-          hirighted: true,
+          highlighted: true,
           text: "이후 한일 위안부 TF 결과 발표와 관련해 문재인 대통령이 "
         },
         {
-          hirighted: false,
+          highlighted: false,
           text:
             "“피해자를 배제하고 한일 정부가 서로 요구조건을 주고 받는 방식으로 문제 해결을 도모한 것은 분명한 잘못”, “일본이 진실을 인정하고 피해자 할머님들께 진심을 다해 사죄하며, 그런 일이 다시는 일어나지 않도록 국제사회와 함께 노력할 때 할머니들도 일본을 용서할 수 있을 것이다. 그것이 완전한 일본군 위안부 문제의 해결”이라는 발언을 하며 "
         },
         {
-          hirighted: true,
+          highlighted: true,
           text: "한일 합의의 문제점에 대해 언급했다. "
         }
       ]
