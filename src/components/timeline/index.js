04a3191f6b0c912f949e1e0d0efd40fe92e2cca6
Choi Sumin <greatSumini@gmail.com>
2019-12-07 23:12:42 +0900

    [Feature] add arrow key handling - can use left, right key to move yearIndex

--- src/components/timeline/index.js
+++ src/components/timeline/index.js
@@ -6,31 +6,7 @@ import TIMELINE_DATA from "../../data/timeline";
 import Card from "./card";
 
 export default function Timeline(props) {
-  const { year, setYear } = props;
-
-  const move = index => {
-    let newYear = index;
-    if (newYear === 1947) {
-      newYear = 1991;
-    }
-    if (newYear === 1990) {
-      newYear = 1946;
-    }
-    newYear = Math.max(1933, Math.min(2019, newYear));
-    if (newYear > 1945) {
-      props.onPhaseChange(1);
-    } else {
-      props.onPhaseChange(0);
-    }
-    setYear(newYear);
-    window.scroll({
-      top:
-        ((newYear > 1990 ? newYear - 44 : newYear) - 1933) * 108 +
-        window.innerHeight,
-      left: 0,
-      behavior: "smooth"
-    });
-  };
+  const { year, setYear, move } = props;
 
   const getCards = () => {
     let suminArray = [];
