6b57596ffc8092bb5db1d25c26ac5029d738215b
Choi Sumin <greatSumini@gmail.com>
2019-12-07 23:12:06 +0900

    [Feature] add START_SECONDS - 선택된 연도에 맞는 시간부터 재생함

--- src/components/main/content/phase0/index.js
+++ src/components/main/content/phase0/index.js
@@ -1,9 +1,25 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import styled from "styled-components";
 import ReactPlayer from "react-player";
 
 import ContentWrapper from "../wrapper";
 import { useScrollPosition } from "../../../scroll";
+
+const START_SECONDS = [
+  3.5,
+  7,
+  10.5,
+  14,
+  17.25,
+  22.75,
+  28.25,
+  32.75,
+  37.25,
+  41.75,
+  46.25,
+  50.75,
+  55.25
+];
 
 const PAUSE_SECONDS = [
   5.75,
@@ -32,13 +48,24 @@ export default function Phase0(props) {
   const [first, setFirst] = useState(true);
   const [now, setNow] = useState(0);
   const position = useScrollPosition();
-  const player = useRef();
+  const [player, setPlayer] = useState(null);
+
+  const ref = player => {
+    setPlayer(player);
+  };
+
+  useEffect(() => {
+    if (!player) {
+      return;
+    }
+    player.seekTo(START_SECONDS[yearIndex] - 0.3, "seconds");
+    setPlaying(true);
+  }, [yearIndex]);
 
   const handleProgress = state => {
     if (state.playedSeconds >= PAUSE_SECONDS[yearIndex]) {
       if (playing) {
         setPlaying(false);
-        player;
         setNow(state.playedSeconds);
         console.log(state.playedSeconds);
       }
@@ -59,7 +86,7 @@ export default function Phase0(props) {
     <ContentWrapper>
       <Wrapper>
         <ReactPlayer
-          ref={player}
+          ref={ref}
           url={"/video/map.mp4"}
           width="100%"
           height="100%"
